92340a9a19ddaf668637f8efc111133ed1bba42d
Your UserName <aratinaik151997@gmail.com>
2021-12-03 23:05:49 +0530

    Added method to find maximum between three float values

--- FindMaxUsingGenerics/FindMaxUsingGenerics/Maximum.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Maximum.cs
@@ -8,7 +8,7 @@ namespace FindMaxUsingGenerics
 {
     class Maximum
     {
-        public static int FindMax()
+        public static int FindMaxInt()
         {
             Console.Write("Enter First number : ");
             int num1 = int.Parse(Console.ReadLine());
@@ -34,9 +34,39 @@ namespace FindMaxUsingGenerics
             }
             return 0;
         }
-        public static void ShowMax()
+        public static void ShowMaxInt()
         {
-            Console.Write("Maximum number is : " + FindMax());
+            Console.Write("Maximum number is : " + FindMaxInt());
+        }
+        public static float FindMaxFloat()
+        {
+            Console.Write("Enter First number : ");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.Write("Enter Second number : ");
+            float num2 = float.Parse(Console.ReadLine());
+            Console.Write("Enter Third number : ");
+            float num3 = float.Parse(Console.ReadLine());
+            if ((num1.CompareTo(num2) > 0 && (num1.CompareTo(num3) > 0)))
+            {
+                return num1;
+            }
+            else if ((num2.CompareTo(num3) > 0 && (num2.CompareTo(num1) > 0)))
+            {
+                return num2;
+            }
+            else if ((num3.CompareTo(num1) > 0 && (num3.CompareTo(num2) > 0)))
+            {
+                return num3;
+            }
+            else
+            {
+                Console.WriteLine("Numbers are equal");
+            }
+            return 0;
+        }
+        public static void ShowMaxFloat()
+        {
+            Console.Write("Maximum number is : " + FindMaxFloat());
         }
     }
 }
--- FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
@@ -10,8 +10,24 @@ namespace FindMaxUsingGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Find Maximum problem using Generics..!");
-            Maximum.ShowMax();
+            Console.WriteLine("\nWelcome to Find Maximum problem using Generics..!");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("1)Maximum in Integer   2)Maximum in Float");
+            Console.WriteLine("---------------------------------------------------");
+            Console.Write("Enter your choice : ");
+            int choice = int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    Maximum.ShowMaxInt();
+                    break;
+                case 2:
+                    Maximum.ShowMaxFloat();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice !!!");
+                    break;
+            }
             Console.ReadKey();
         }
     }
