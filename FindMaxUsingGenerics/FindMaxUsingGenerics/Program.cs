401b919f13b0174b6397a085a2dec8156821e93a
Your UserName <aratinaik151997@gmail.com>
2021-12-03 22:17:47 +0530

    Welcome to Find maximum problem using generics

---
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum problem using Generics..!");
+            Console.ReadKey();
+        }
+    }
+}
