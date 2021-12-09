ea7943c95203b1904044aa52abfaf8fd464aa660
Your UserName <aratinaik151997@gmail.com>
2021-12-09 15:00:02 +0530

    Added One generic method to compare objects of string, int, andfloat

--- FindMaxUsingGenerics/FindMaxUsingGenerics/Maximum.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Maximum.cs
@@ -6,68 +6,29 @@ using System.Threading.Tasks;
 
 namespace FindMaxUsingGenerics
 {
-    class Maximum
+    public class Maximum<T> where T : IComparable 
     {
-        public static int FindMaxInt()
+        public static T FindMax()
         {
-            Console.Write("Enter how many numbers you want to comapare : ");
+            Console.Write("Enter how many elements you want to comapare : ");
             int size = int.Parse(Console.ReadLine());
-            int[] array = new int[size];
+            T[] array = new T[size];
             int count = 1;
-            for(int i=0;i<size;i++)
+            for (int i=0;i<size;i++)
             {
-                Console.Write("Enter {0} number : ",count);                
-                array[i]= int.Parse(Console.ReadLine());
+                Console.Write("Enter {0} element : ",count);
+                string input =Console.ReadLine();
+                array[i]=(T)Convert.ChangeType(input, typeof(T));
                 count++;
             }
             Array.Sort(array);
-            int max = array[size - 1];
+            T max = array[size - 1];
             return max;
         }
-        public static void ShowMaxInt()
+        public static void ShowMax()
         {
-            Console.Write("Maximum number is : " + FindMaxInt());
+            Console.Write("Maximum element is : " + FindMax());
         }
-        public static float FindMaxFloat()
-        {
-            Console.Write("Enter how many numbers you want to comapare : ");
-            int size = int.Parse(Console.ReadLine());
-            float[] array = new float[size];
-            int count = 1;
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write("Enter {0} number : ", count);
-                array[i] = float.Parse(Console.ReadLine());
-                count++;
-            }
-            Array.Sort(array);
-            float max = array[size - 1];
-            return max;
-        }
-        public static void ShowMaxFloat()
-        {
-            Console.Write("Maximum number is : " + FindMaxFloat());
-        }
-        public static string FindMaxString()
-        {
-            Console.Write("Enter how many strings you want to comapare : ");
-            int size = int.Parse(Console.ReadLine());
-            string[] array = new string[size];
-            int count = 1;
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write("Enter {0} string : ", count);
-                array[i] =Console.ReadLine();
-                count++;
-            }
-            Array.Sort(array);
-            string max = array[size - 1];
-            return max;
-
-        }
-        public static void ShowMaxString()
-        {
-            Console.Write("Maximum string is : " + FindMaxString());
-        }
+       
     }
 }
--- FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
@@ -12,20 +12,20 @@ namespace FindMaxUsingGenerics
         {
             Console.WriteLine("\nWelcome to Find Maximum problem using Generics..!");
             Console.WriteLine("---------------------------------------------------");
-            Console.WriteLine("1)Maximum in Integer   2)Maximum in Float    3)Maximum in String");
+            Console.WriteLine("1)Maximum in Integer   2)Maximum in Float    3)Maximum in String ");
             Console.WriteLine("---------------------------------------------------");
             Console.Write("Enter your choice : ");
             int choice = int.Parse(Console.ReadLine());
             switch(choice)
             {
                 case 1:
-                    Maximum.ShowMaxInt();
+                    Maximum<int>.ShowMax();
                     break;
                 case 2:
-                    Maximum.ShowMaxFloat();
+                    Maximum<float>.ShowMax();
                     break;
                 case 3:
-                    Maximum.ShowMaxString();
+                    Maximum<string>.ShowMax();
                     break;
                 default:
                     Console.WriteLine("Invalid Choice !!!");
